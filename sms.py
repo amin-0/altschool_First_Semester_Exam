02e2a7fee38239cdf3b126da60834b190db97126
amin <owodunniadeyinka@gmail.com>
2024-08-31 11:13:13 +0100

    added some methods

--- sms.py
+++ sms.py
@@ -1,6 +1,6 @@
 
 class Person:
-    def __init__(self, name: str, id_number: int) -> None:
+    def __init__(self, name: str, id_number: str) -> None:
         self.name = name
         self.id_number = id_number
     
@@ -8,10 +8,10 @@ class Person:
         return f"name: {self.name}, id_number: {self.id_number}"
 
 class Student(Person):
-    def __init__(self, name: str, id_number:int, major:str) -> None:
+    def __init__(self, name: str, id_number:str, major:str) -> None:
         super().__init__(name, id_number)
         self.major = major
-    
+
     def __str__(self) -> str:
         return f"name: {self.name}, id_number: {self.id_number}, major: {self.major}"
 
@@ -47,4 +47,51 @@ class StudentManagementSystem:
         self.courses: list[Course] = []
         self.instructors: list[Instructor] = []
         self.enrollments: list[Enrollment] = []
+    
+    def __str__(self) -> str:
+        return f"This is a student management system"
+    
+    def add_student(self, student : Student):
+        self.students.append(student)
 
+    def remove_student(self, student_id : str):
+        #Add a try catch to check if the id is in the sms
+        student = [student for student in self.students if student.id_number == student_id ][0]
+        self.students.remove(student)
+    
+    def get_students(self) -> list[Student] :
+        return [student.name for student in self.students]
+
+    def update_students(self, id : str, updated_name : str, updated_major : str):
+        student = [student for student in self.students if student.id_number == id ][0]
+        student.name = updated_name
+        student.major = updated_major
+    
+    def update_instructor(self, id : str, updated_name : str, updated_department : str):
+        instructor = [instructor for instructor in self.instructors if instructor.id_number == id ][0]
+        instructor.name = updated_name
+        instructor.department = updated_department
+        
+    def get_instructors(self) -> list[Instructor] :
+        return [instructor.name for instructor in self.instructors]
+    
+    def add_course(self, course : Course):
+        self.course.append(course)
+
+    def remove_course(self, course_id : str):
+        course = [course for course in self.course if course.id_number == course_id ][0]
+        self.course.remove(course)
+    
+    def update_course(self, id : str, updated_name : str, updated_major : str):
+        student = [student for student in self.students if student.id_number == id ][0]
+        student.name = updated_name
+        student.major = updated_major
+
+    def add_course():
+        pass
+
+    def remove_course():
+        pass
+
+    def update_course():
+        pass
